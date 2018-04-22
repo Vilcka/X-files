cf44f590e34b2c88fd56c8ecb9cc121bd4fb5982
vilca <vilc86@hotmail.com>
2018-04-23 00:25:47 +0200

    yolo

--- index.php
+++ index.php
@@ -2,18 +2,18 @@
 <?php
     $path = realpath('files');
     $objects = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($path),RecursiveIteratorIterator::SELF_FIRST);
-    foreach ($objects as $name => $object) {
-        if((basename($object) != '.') && (basename($object) != '..')) {
+    $files = [];
+    foreach ($objects as $name ) {
+        if((basename($name) != '.') && (basename($name) != '..')) {
+            $files[] = basename($name->getPathname());
             if (is_dir($name)) {
                 echo '<strong><a href="'. $name.'">' . ucfirst(basename($name)) . '</a></strong><br/>';
             }else {
-                echo '<a class="files" href="'. $name .'">'. basename($object) . '</a><br/>';
+                echo '<a class="files" href="'. $name .'">'. basename($name) . '</a><br/>';
             }
-
 
         }
     }
-
 
 ?>
 

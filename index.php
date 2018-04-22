3e9552e5917b96ec40ced76acb9d59fefa16388b
vilca <vilc86@hotmail.com>
2018-04-22 23:56:59 +0200

    Arborescendes du dossier files

--- index.php
+++ index.php
@@ -1,5 +1,20 @@
 <?php include('inc/head.php'); ?>
+<?php
+    $path = realpath('files');
+    $objects = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($path),RecursiveIteratorIterator::SELF_FIRST);
+    foreach ($objects as $name => $object) {
+        if((basename($object) != '.') && (basename($object) != '..')) {
+            if (is_dir($name)) {
+                echo '<strong><a href="'. $name.'">' . ucfirst(basename($name)) . '</a></strong><br/>';
+            }else {
+                echo '<a class="files" href="'. $name .'">'. basename($object) . '</a><br/>';
+            }
 
-    C'est ici que tu vas devoir afficher le contenu de tes repertoires et fichiers.
+
+        }
+    }
+
+
+?>
 
 <?php include('inc/foot.php'); ?>

67be73af80dc49a6235d990596171c27e0474542
vilca <vilc86@hotmail.com>
2018-04-29 17:56:57 +0200

    sa marche en theorie

--- inc/head.php
+++ inc/head.php
@@ -3,6 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <title>FBI | X-Files archives</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+          integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <link rel="stylesheet" href="assets/bootstrap.css"/>
     <link rel="stylesheet" href="assets/custom.css"/>
 </head>
--- index.php
+++ index.php
@@ -1,35 +1,57 @@
 <?php include('inc/head.php'); ?>
 <?php
 if (isset($_POST['contenu'])) {
-    $fichier = $files[1];
+    $fichier = $_POST['file'];
     $file = fopen($fichier, "w");
-    fwrite($fichier, $_POST['contenu']);
-    fclose($fichier);
+    fwrite($file, $_POST['contenu']);
+    fclose($file);
+}
+
+if (isset($_GET['delete'])) {
+    if (is_dir($_GET['delete'])) {
+        rmdir($_GET['delete']);
+    } else {
+        unlink($_GET['delete']);
+    }
 }
 
 
-    $path = realpath('files');
-    $objects = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($path),RecursiveIteratorIterator::SELF_FIRST);
-    $files = [];
-    foreach ($objects as $name => $object) {
-        if ((basename($name) != '.') && (basename($name) != '..')) {
-            $files[] = $name;
-            if (is_dir($name)) {
-                echo '<strong>' . ucfirst(basename($name)) .'</strong><br/>';
-            }else {
-                echo '<a class="files" href="?f='. dirname($name) . $name .'">'. basename($name) . '</a><br/>';
-            }
+$path = realpath('files');
+$objects = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($path),RecursiveIteratorIterator::SELF_FIRST);
+$pathInfo = [];
+foreach ($objects as $object) {
+    if ((basename($object) != '.') && (basename($object) != '..')) {
+        $pathInfo[$object->getFileName()] = pathinfo($object);
+        if (is_dir($object)) {
+            echo ' <strong>' . ucfirst(basename($object)) .'</strong>
+                    <a href="?delete=' . $object->getPathName() . '"><span class="glyphicon glyphicon-remove delete" aria-hidden="true"></span></a>  
+                    <br/>';
+        } else {
+            echo '  <input type="hidden" name="delete" value="' . $object->getPathName() . '">
+                    <a class="files" href="?fileEdit=' . $object . '">'. basename($object) . '</a>
+                    <a href="?delete=' . $object->getPathName() . '"><span class="glyphicon glyphicon-remove delete" aria-hidden="true"></span></a>
+                    <br/>';
         }
     }
-    var_dump($files);
-
-$fichier = $files[1];
-$contenu = file_get_contents($fichier);
-
+}
+if (isset($_GET['fileEdit'])) {
+    $extension = $pathInfo[basename($_GET['fileEdit'])]['extension'];
+    if ($extension == 'jpg') {
+        $errorsFileExtension = '<h4>Vous ne pouvez editez que les fichier txt ou html !</h4>';
+    } else {
+        $contenu = file_get_contents($_GET['fileEdit']);
 ?>
 <form method="POST" action="index.php">
     <textarea name="contenu"><?= $contenu ?></textarea>
+    <input type="hidden" name="file" value="<?= $_GET['fileEdit'] ?>">
     <input type="submit" value="Envoyer">
 </form>
+<?php
+//Fermeture du else puis du bloc if parent
+    }
+} ?>
 
+<?php if (isset($errorsFileExtension)){
+    echo $errorsFileExtension;
+} ?>
 <?php include('inc/foot.php'); ?>
